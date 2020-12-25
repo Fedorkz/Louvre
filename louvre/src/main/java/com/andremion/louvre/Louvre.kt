f265dc6a5220130f4cd1a19fd3a3389a2d842d02
Fedor Sokovikov <FedorkZ@gmail.com>
2020-12-25 02:57:35 +0200

    Add videos to library - two use cases with and without video - open interface

--- louvre/src/main/java/com/andremion/louvre/Louvre.kt
+++ louvre/src/main/java/com/andremion/louvre/Louvre.kt
@@ -105,12 +105,13 @@ class Louvre {
         return this
     }
 
-    fun open() {
+    @JvmOverloads
+    fun open(includeVideo: Boolean = false) {
         require(mRequestCode != -1) { "You need to define a request code in setRequestCode(int) method" }
         if (mActivity != null) {
-            GalleryActivity.startActivity(mActivity!!, mRequestCode, mMaxSelection, mSelection, false, *mMediaTypeFilter)
+            GalleryActivity.startActivity(mActivity!!, mRequestCode, mMaxSelection, mSelection, includeVideo, *mMediaTypeFilter)
         } else {
-            GalleryActivity.startActivity(mFragment!!, mRequestCode, mMaxSelection, mSelection, false, *mMediaTypeFilter)
+            GalleryActivity.startActivity(mFragment!!, mRequestCode, mMaxSelection, mSelection, includeVideo, *mMediaTypeFilter)
         }
     }
 }
